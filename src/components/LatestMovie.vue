936fb5a01cf07d15ebf79aa0b52f95c6c945ec0b
aniaska4 <aniaska4@wp.pl>
2020-02-17 15:46:44 +0100

    layout main page

--- src/components/LatestMovie.vue
+++ src/components/LatestMovie.vue
@@ -1,6 +1,15 @@
-<template>
-  <div>
-  </div>
+<template lang="pug">
+  .mainBox
+    .container
+      .movieLists
+        .movieLists__movie(v-for="item in allMovies" :key="item.title")
+          .movieLists__movie-img
+            img(:src="item.Poster")
+          .movieLists__movie__details
+            .movieLists__movie-title Title: {{item.Title}}
+            .movieLists__movie-year Year: {{item.Year}}
+            .movieLists__movie-type Type: {{item.Type}}
+            .movieLists__movie-imdbID {{item.imdbID}}
 </template>
 
 <script>
@@ -9,7 +18,7 @@ export default {
   name: 'LatestMovie',
   data() {
     return {
-
+      allMovies: [],
     }
   },
   mounted () {
@@ -17,7 +26,7 @@ export default {
     .get('http://www.omdbapi.com/?s=mummy&apikey=8d840397&page=1&type=movie&Content-Type=application/json')
     .then(response => {
       console.log(response)
-      this.wholeResponse = response.data.Search
+      this.allMovies = response.data.Search
     })
     .catch(error => {
       console.log(error)
@@ -27,4 +36,32 @@ export default {
 </script>
 
 <style scoped>
+.container{
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+}
+
+.movieLists{
+  display: flex;
+  flex-wrap: wrap
+}
+
+.movieLists__movie{
+  margin: 10px;
+}
+
+img{
+  width: 300px;
+  height: 450px;
+}
+.movieLists__movie__details{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.movieLists__movie-title{
+  /* font-weight: 600 */
+}
 </style>
