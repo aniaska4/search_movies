c7b9e2a76a7b88476b016ae4dca55c52f4cd0aa5
aniaska4 <aniaska4@wp.pl>
2020-02-17 11:44:23 +0100

    add components

---
+++ src/components/LatestMovie.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'LatestMovie',
+  data() {
+    return {
+
+    }
+  },
+  mounted () {
+  axios
+    .get('http://www.omdbapi.com/?s=mummy&apikey=8d840397&page=1&type=movie&Content-Type=application/json')
+    .then(response => {
+      console.log(response)
+      this.wholeResponse = response.data.Search
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+}
+</script>
+
+<style scoped>
+</style>
